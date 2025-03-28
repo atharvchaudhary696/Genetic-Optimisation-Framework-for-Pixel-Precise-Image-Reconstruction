5203a01f2f6cb210063bfbe70fbfb2c92a7674f1
atharvchaudhary696 <119797158+atharvchaudhary696@users.noreply.github.com>
2025-03-28 14:48:56 +0530

    Refactor image processing scripts: consolidate functionality into testing directory and remove obsolete files

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 from Modules import ImageParameters
-from script import Display_Image 
+from testing import Display_Image 
 from Modules import Image_Genetic_Algorithm
 # from  Modules import Filter
 def Main(Apprunning=False,image_path="fruit.jpg",FolderPath="Modules",display=False):
@@ -10,4 +10,4 @@ def Main(Apprunning=False,image_path="fruit.jpg",FolderPath="Modules",display=Fa
             img=FolderPath+"/Finalimg.png"
             Display_Image.display_image(img)
 if __name__ == "__main__":
-    Main(Apprunning=True,display=True,image_path="Output\InputImages\\fruit.jpg",FolderPath="Output\OutputImages")
+    Main(Apprunning=True,display=True,image_path=r"data\raw\fruit.jpg",FolderPath=r"data\output")
